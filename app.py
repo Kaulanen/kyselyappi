57d15631ac231a2c5ff3f0e8d7872ab19ba2b235
Kaulanen <elsa.kaulanen@gmail.com>
2025-11-28 10:24:56 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,39 +1,11 @@
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Viestit vaihtoehdoille
-messages_A = [
-    "Kiitos! Valitsit Vaihtoehto A.",
-    "Hienoa, että pidät A:sta!",
-    "A on suosittu valinta!"
-]
-
-messages_B = [
-    "Kiitos! Valitsit Vaihtoehto B.",
-    "B on hyvä valinta!",
-    "B:llä on oma kannattajakuntansa!"
-]
-
-click_count_A = 0
-click_count_B = 0
-
-@app.route("/", methods=["GET", "POST"])
+@app.route('/')
 def index():
-    global click_count_A, click_count_B
-    message = None
+    return render_template('index.html')
 
-    if request.method == "POST":
-        vote = request.form.get("vote")
-        if vote == "A":
-            message = messages_A[click_count_A % len(messages_A)]
-            click_count_A += 1
-        elif vote == "B":
-            message = messages_B[click_count_B % len(messages_B)]
-            click_count_B += 1
+if __name__ == '__main__':
+    app.run(debug=True)
 
-    return render_template("index.html", message=message)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
